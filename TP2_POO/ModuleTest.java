da5b3d779556ed7e11d1c4ebaa914847bc8949b8
WololoBzh <33237516+wololobzh@users.noreply.github.com>
2025-10-24 11:03:46 +0200

    Add files via upload

---
+++ TP2_POO/ModuleTest.java
@@ -0,0 +1,83 @@
+public class ModuleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//A vous de constituer votre module de test afin d'obtenir le résultats attendus
+		
+		//creation de 2,3 auteurs
+		//TODO Votre code ici...
+		
+		
+		System.out.println("** Les auteurs");
+		System.out.println("**************");
+		//TODO Votre code ici...
+		
+		
+		//creation de 2,3 editeurs
+		//TODO Votre code ici...
+		
+		
+		System.out.println("** Les editeurs");
+		System.out.println("***************");
+		//TODO Votre code ici...
+		
+		
+		//creation de 2,3 livres
+		//TODO Votre code ici...
+		
+		
+		System.out.println("** Les livres");
+		System.out.println("*************");
+		//TODO Votre code ici...
+		
+		
+		//creation de 2,3 cd
+		//TODO Votre code ici...
+		
+		System.out.println("** Les cd");
+		System.out.println("*********");
+		//TODO Votre code ici...
+		
+		
+		//ajouter les produits au catalogue
+		//TODO Votre code ici...
+		
+		
+		//tester le polymorphisme (en affichant un livre issu du catalogue avec son prix de vente)
+		System.out.println("** polymorphisme");
+		System.out.println("****************");
+		//TODO Votre code ici...
+		
+		
+		//ajouter une piste à un cd issu du catalogue
+		//TODO Votre code ici...
+		
+		
+		//lister l'intégralité du catalogue
+		System.out.println("** Le catalogue complet");
+		System.out.println("***********************");
+		//TODO Votre code ici...
+		
+		
+		//extraire les produits par type du catalogue
+		System.out.println("** Les livres au catalogue");
+		System.out.println("**************************");
+		//TODO Votre code ici...
+		
+		
+		//extraire les produits par type du catalogue
+		System.out.println("** Les cds au catalogue");
+		System.out.println("***********************");
+		//TODO Votre code ici...
+		
+
+		
+		System.out.println("Appuyez sur entrée pour sortir du test...");
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+		
+	}
+
+}
